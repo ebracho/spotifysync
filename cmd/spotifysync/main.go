079f09f41d08972e9b6e4090ad7db941d36a75b3
Eddie Bracho <eddiebracho@gmail.com>
2020-08-21 16:22:39 -0700

    tls

--- cmd/spotifysync/main.go
+++ cmd/spotifysync/main.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ebracho/spotifysync"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
-func main() {
+func serveLocal() {
 	cfg, err := spotifysync.NewConfigFromFile("./config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := cfg.Save(); err != nil {
-		log.Println(err)
-	}
-	srv := spotifysync.SpotifySyncServer{
+	ss := spotifysync.SpotifySyncServer{
 		SpotifyClient: spotifysync.NewSpotifyClient(cfg),
 		Cfg:           cfg,
 	}
-	srv.RegisterHandlers()
+	ss.RegisterHandlers()
 	if err := http.ListenAndServe(cfg.ListenAddress, nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func serveProd() {
+	cfg, err := spotifysync.NewConfigFromFile("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ss := spotifysync.SpotifySyncServer{
+		SpotifyClient: spotifysync.NewSpotifyClient(cfg),
+		Cfg:           cfg,
+	}
+	ss.RegisterHandlers()
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("example.com"), //Your domain here
+		Cache:      autocert.DirCache("certs"),            //Folder for storing certificates
+	}
+	server := &http.Server{
+		Addr:    ":https",
+		Handler: http.DefaultServeMux,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if _, err := os.Stat("certs"); err == nil {
+		serveProd()
+	} else {
+		serveLocal()
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ebracho/spotifysync
 
 go 1.14
 
-require golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+require (
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
