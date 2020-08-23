ee1b6b4b24d6fa9d4cf90cd272bfe3a32cae1f01
Eddie Bracho <root@spotify.kazlock.net>
2020-08-23 05:34:11 +0000

    remove old container before starting new prod container

--- run-prod.sh
+++ run-prod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash +x
 docker build . -t spotifysync:latest
+docker stop spotifysync || true && docker rm spotifysync || true # stop the old container
 docker run -p 80:80 -p 443:443 -v "$(pwd)/config.json":/app/config.json -v "$(pwd)/certs/":/app/certs/ -d --name spotifysync spotifysync:latest
